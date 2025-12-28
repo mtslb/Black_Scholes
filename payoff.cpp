fe7fa203988a47746a3dd946b3872a15b410ff18
Lilou <lilou.malfoy12@gmail.com>
2025-12-28 12:43:14 +0100

    normalement il est fini

--- payoff.cpp
+++ payoff.cpp
@@ -1,1 +1,48 @@
 #include "payoff.hpp"
+#include <cmath>
+#include <algorithm>
+#include <stdexcept>
+
+
+Option::Option(double K, double L, double r, double T) //Constructeur de la classe Option
+    : K_(K), L_(L), r_(r), T_(T) {}
+
+Option::~Option() {} //Destructeur virtuel de la classe Option
+
+
+
+//Implémentation des méthodes de la classe Call
+
+Call::Call(double K, double L, double r, double T) //constructeur de la classe Call
+    : Option(K, L, r, T) {} //appel du constructeur de la classe mère Option
+double Call::payoff(double S) const { //Implémentation de la méthode payoff pour Call
+    if (S > L_) {
+        throw std::out_of_range("S is out of range");
+    }
+    return std::max(S - K_, 0.0);
+}
+double Call::boundary_condition_low(double t) const { //Implémentation de la condition à la limite S=0 pour Call
+    return 0.0;
+}
+double Call::boundary_condition_high(double t) const { //Implémentation de la condition à la limite S=L pour Call
+    return L_ - K_ * std::exp(-r_ * (t -T_));
+}
+
+
+
+//Implémentation des méthodes de la classe Put
+
+Put::Put(double K, double L, double r, double T) //constructeur de la classe Put
+    : Option(K, L, r, T) {} //appel du constructeur de la classe mère Option
+double Put::payoff(double S) const { //Implémentation de la méthode payoff pour Put
+    if (S > L_) {
+        throw std::out_of_range("S is out of range");
+    }
+    return std::max(K_ - S, 0.0);  
+}
+double Put::boundary_condition_low(double t) const { //Implémentation de la condition à la limite S=0 pour Put
+    return K_ * std::exp(-r_ *(T_ - t)); 
+}
+double Put::boundary_condition_high(double t) const { //Implémentation de la condition à la limite S=L pour Put
+    return 0.0;
+}
