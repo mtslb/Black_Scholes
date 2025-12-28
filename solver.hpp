0702f056d42b5bf2cf2497cf82dacc36accfc0ce
Lilou <lilou.malfoy12@gmail.com>
2025-12-28 18:26:34 +0100

    j'ai corrigé les erreurs et rajouté des trucs

--- solver.hpp
+++ solver.hpp
@@ -5,36 +5,44 @@
 #include "edp.hpp"
 
 
-class solver {
-    private:
+class Solver {
+    protected: //pour que les classes filles y aient accès
         double dt_; // Pas de temps
         double dS_; // Pas d'espace
-        std::vector<double>& t_; // Valeurs de temps t pour lesquelles on calcule la solution
-        std::vector<double>& S_; // Valeurs de l'actif S pour lesquelles on calcule la solution
+        EDP&  edp_; // Référence vers l'EDP à résoudre
+        int N_; // Nombre de points en espace
+        int M_; // Nombre de points en temps
+        std::vector<double> t_; // Valeurs de temps t pour lesquelles on calcule la solution
+        std::vector<double> S_; // Valeurs de l'actif S pour lesquelles on calcule la solution
+        std::vector<std::vector<double>> V_; // Matrice des prix
 
     public:
-        solver(int L, int N, int T, int M, std::vector<double>& t, std::vector<double>& S)
-            : t_(t), S_(S) {
-            dt_ = T / M;
-            dS_ = L / N;
-        }
-
+        Solver(EDP& edp, int N, int M);
+        //     : t_(t), S_(S) {
+        //     dt_ = T / M;
+        //     dS_ = L / N;
+        // }
         virtual void solve() = 0;
     };
 
-class differences_finies_implicites : public solver {
-    public:
-        differences_finies_implicites(int L, int N, int T, int M, std::vector<double>& t, std::vector<double>& S)
-            : solver(L, N, T, M, t, S) {}
 
+
+//classe Differences finies implicites
+class Differences_finies_implicites : public Solver { //heritage de la classe Solver
+    public:
+        Differences_finies_implicites(EDP& edp, int N, int M);
+            // : Solver(edp, N, M) {}
         void solve() override;
     };
 
-class cranck_nicolson : public solver {
-    public:
-        cranck_nicolson(int L, int N, int T, int M, std::vector<double>& t, std::vector<double>& S)
-            : solver(L, N, T, M, t, S) {}
 
+
+
+//classe Cranck-Nicolson 
+class Cranck_nicolson : public Solver { //heritage de la classe Solver
+    public:
+        Cranck_nicolson(EDP& edp, int N, int M);
+            // : Solver(edp, N, M) {}
         void solve() override;
     };
 
