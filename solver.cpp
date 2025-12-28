210cf55d79f5936b3df91923046a1ade123922d6
Lilou <lilou.malfoy12@gmail.com>
2025-12-28 18:40:37 +0100

    je finirais plus tard ou demain là j'ai la flemme

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,8 @@
 #include "solver.hpp"
 
-cranck_nicolson::solve(
+Solver::Solver(EDP& edp, int N, int M) : edp_(edp), N_(N), M_(M) {
+    //on récupère T et L de l'EDP
+    int T= edp_.getT(); 
+    int L= edp_.getL();
 
-    
-);
+}
