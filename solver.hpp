c802ec633031e797f740d667548e3c153e638d3c
Lilou Malfoy <lilou.malfoy12@gmail.com>
2026-01-01 12:30:48 +0100

    à commenter et verifier

--- solver.hpp
+++ solver.hpp
@@ -1,48 +1,39 @@
 #ifndef SOLVER_HPP
 #define SOLVER_HPP
 
-#include <vector>
 #include "edp.hpp"
+#include <vector>
 
+class solver {
+protected:
+    EDP& edp_;
+    int N_, M_;
+    double dt_, dS_;
+    std::vector<double> S_, t_;
+    std::vector<std::vector<double>> v_;
 
-class Solver {
-    protected: //pour que les classes filles y aient accès
-        double dt_; // Pas de temps
-        double dS_; // Pas d'espace
-        EDP&  edp_; // Référence vers l'EDP à résoudre
-        int N_; // Nombre de points en espace
-        int M_; // Nombre de points en temps
-        std::vector<double> t_; // Valeurs de temps t pour lesquelles on calcule la solution
-        std::vector<double> S_; // Valeurs de l'actif S pour lesquelles on calcule la solution
-        std::vector<std::vector<double>> V_; // Matrice des prix
+public:
+    solver(EDP& edp, int N, int M);
+    virtual void solve() = 0;
+    std::vector<std::vector<double>> get_results() const { return v_; }
+    
+    // L'algorithme de Thomas adapté
+    std::vector<double> thomas_algorithm(const std::vector<double>& a, 
+                                        const std::vector<double>& b, 
+                                        const std::vector<double>& c, 
+                                        const std::vector<double>& d);
+};
 
-    public:
-        Solver(EDP& edp, int N, int M);
-        //     : t_(t), S_(S) {
-        //     dt_ = T / M;
-        //     dS_ = L / N;
-        // }
-        virtual void solve() = 0;
-        ~Solver() = default;
-    };
+class cranck_nicolson : public solver {
+public:
+    cranck_nicolson(EDP& edp, int N, int M) : solver(edp, N, M) {}
+    void solve() override;
+};
 
+class implicite_solver : public solver {
+public:
+    implicite_solver(EDP& edp, int N, int M) : solver(edp, N, M) {}
+    void solve() override;
+};
 
-//classe Differences finies implicites
-class Differences_finies_implicites : public Solver { //heritage de la classe Solver
-    public:
-        Differences_finies_implicites(EDP& edp, int N, int M);
-            // : Solver(edp, N, M) {}
-        void solve() override;
-    };
-
-
-
-//classe Cranck-Nicolson 
-class Cranck_nicolson : public Solver { //heritage de la classe Solver
-    public:
-        Cranck_nicolson(EDP& edp, int N, int M);
-            // : Solver(edp, N, M) {}
-        void solve() override;
-    };
-
-#endif // SOLVER_HPP
+#endif
